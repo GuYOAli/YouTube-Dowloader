280f2c3003e65f9560bcf9248c60608761312268
GuledAli <guledali@guledify.com>
2022-09-06 03:34:10 -0700

    Getting information of the video.

---
+++ YouTubeDownloader/YouTube_Downloader.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+
+link = input("Past Your Link here: ").strip()
+
+yt_link = YouTube(link)
+
+# Title of video
+print("Title: ", yt_link.title)
+# Number of views of video
+print("Number of views: ", yt_link.views)
+# Length of the video
+print("Length of video: ", yt_link.length, " mintues")
+# Description of video
+print("Description: ", yt_link.description)
+# Rating
+print("Ratings: ", yt_link.rating)
